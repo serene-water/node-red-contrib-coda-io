b488c1def38ed87dd9df64fddc6f0a94484eed88
Mori Sugimoto <morihiro.sugimoto@gmail.com>
2018-11-28 19:34:38 +0100

    The get node works now. Also added README.md and resized the icon

---
+++ README.md
@@ -0,0 +1,28 @@
+# Node-RED nodes for coda
+
+[coda](https://coda.io/ "coda") is a powerful  online tool that allows you to build complex and interactive documents quickly.
+
+With [coda APIs](https://coda.io/developers/apis/v1beta1 "coda.io APIs"), you can interact with tables within Coda documents. The nodes included in this package allow you to easily work with Coda API using Node-RED.
+
+## How to use the node 'Get coda tables/rows'
+### 1. Preparation
+The node comes with fields for you to enter the following information:
+  - Coda API authentication key
+  - Document ID
+  - Table ID
+
+To find your document ID, use [this tool](https://coda.io/developers/apis/v1beta1#doc-ids).
+
+### 2. Find out table IDs
+To find the table name, send leave the 'Table ID' field blank.
+
+Connect an inject node to the coda node, then connect a HTTP request function node so that the output of the coda node is passed on to it. Finally output the message to a debug node. Here's an example of the flow:
+
+```[{"id":"69eb5e38.bc12b","type":"tab","label":"Flow 2","disabled":false,"info":""},{"id":"2505aded.ab29b2","type":"debug","z":"69eb5e38.bc12b","name":"","active":true,"tosidebar":true,"console":false,"tostatus":false,"complete":"payload","x":270,"y":200,"wires":[]},{"id":"7c547c80.9444e4","type":"inject","z":"69eb5e38.bc12b","name":"","topic":"","payload":"","payloadType":"date","repeat":"","crontab":"","once":false,"onceDelay":0.1,"x":160,"y":80,"wires":[["b8149537.635058"]]},{"id":"b8149537.635058","type":"coda-io-get-table","z":"69eb5e38.bc12b","bearer_api_token":"[API TOKEN HERE]","doc_id":"[DOC ID HERE]","table_id":"","get_rows":false,"name":"","x":220,"y":120,"wires":[["5d41bbb8.af3084"]]},{"id":"5d41bbb8.af3084","type":"http request","z":"69eb5e38.bc12b","name":"","method":"GET","ret":"obj","url":"","tls":"","x":230,"y":160,"wires":[["2505aded.ab29b2"]]}]```
+
+Coda's response is found in `msg.payload`. `msg.payload.items` is an array of all the tables found in the document. If the debug message gets cut off and you cannot see all the tables, simply increase the value of `debugMaxLength` in `settings.js`.
+
+### 3. Get rows from a table
+Once you found the ID of the table from which you want to get records, add the table ID to the coda node and tick the 'Get rows' checkbox.
+
+Coda's response will be under `msg.payload.items`, which includes metadata of each row. Values of each row will be found in `msg.payload.items.values`.
--- coda-io-get-table.js
+++ coda-io-get-table.js
@@ -1,57 +1,75 @@
+var _ = require('lodash');
+
 module.exports = function(RED) {
-    function CodaIoTableNode(config) {
-        RED.nodes.createNode(this,config);
+
+    function CodaIoTableNode(n) {
+
+        RED.nodes.createNode(this, n);
         var node = this;
         node.on('input', function(msg) {
 
+            var payload = typeof msg.payload === 'object' ? msg.payload : {};
+            var attrs = ['bearer_api_token', 'doc_id', 'table_id', 'get_rows'];
+            var val = attrVal = '';
+
+            for (var attr of attrs) {
+
+                // Attempt matching only if the value is not boolean or null,
+                // as it will fail otherwise
+                if (_.isBoolean(n[attr]) === false && _.isNull(n[attr]) === false) {
+                    attrVal = n[attr].match(/msg\.[a-zA-Z0-9-_.]+/g);
+                }
+
+                // Request for rows if the user desires
+                if(attr == 'get_rows') {
+                    n[attr] ? payload[attr] = true : false;
+                }
+
+                // Get the object member if a match is found in a user-provided string
+                else if (attrVal !== null) {
+
+                    // Evaluates the string to return an object member
+                    val = eval(attrVal[0]);
+
+                    // Only accept number or string data types
+                    if (_.isNumber(val) || _.isString(val)) {
+                        payload[attr] = val;
+                    }
+                    else {
+                        console.log("Type error: Type of the value for " + attrVal + " must be string or number. " + typeof(val) + " was given instead. Field name: " + attr + ". The value wasn't included in the query string");
+                    }
+                }
+                // No checks necessary
+                else {
+                    payload[attr] = n[attr];
+                }
+            }
+
+            // HTTP header
+            msg.headers = {};
+            msg.headers['Authorization'] = 'Bearer ' + payload.bearer_api_token;
+
+            let req = {
+                'docs': payload.doc_id,
+                'tables': payload.table_id
+            }
+            // Get rows only if the table ID is given and the user requests for it
+            if (payload.get_rows === true && payload.table_id != '') {
+                req.rows = '';
+            }
+
+            // Construct a query string
+            let reqStr;
+            for(var i in req){
+                reqStr = (reqStr == undefined ? i : reqStr + '/' + i);
+                reqStr = (req[i] == null ? reqStr + '' : reqStr + '/' + req[i]);
+            }
+            // Keep reqStr in case it is needed later
             msg.coda = {};
-////
+            msg.coda.reqStr = reqStr;
 
+            msg.url = 'https://coda.io/apis/v1beta1/' + reqStr;
 
-var creds = RED.nodes.getNode(n.creds);
-
-var payload = typeof msg.payload === 'object' ? msg.payload : {};
-
-var attrs = ['bearer_api_token', 'doc_id', 'table_id', 'get_rows'];
-
-var attrVal = '';
-var val = ''
-for (var attr of attrs) {
-
-    // Attempt matching only if the value is not boolean or null
-    // as it will fail otherwise
-    if (_.isBoolean(n[attr]) === false && _.isNull(n[attr]) === false) {
-        attrVal = n[attr].match(/msg\.[a-zA-Z0-9-_.]+/g);
-    }
-
-    // Request for rows if the user desires
-    if(attr == 'get_rows') {
-        n[attr] ? payload[attr] = "rows" : '';
-    }
-
-    // Get the object member if a match is found in a user-provided string
-    else if (attrVal !== null) {
-
-        // Evaluates the string to return an object member
-        val = eval(attrVal[0]);
-
-        // Only accept number or string data types
-        if (_.isNumber(val) || _.isString(val)) {
-            payload[attr] = val;
-        }
-        else {
-            node.warn("Type error: Type of the value for " + attrVal + " must be string or number. " + typeof(val) + " was given instead. Field name: " + attr + ". The value wasn't included in the search string");
-        }
-    }
-
-    else {
-        payload[attr] = n[attr];
-    }
-}
-        msg.coda = {};
-        msg.coda.qstr = payload;
-
-////
             node.send(msg);
         });
     }
