688e4968cf22ea562466f0730a0f18105e7526bd
Mori Sugimoto <morihiro.sugimoto@gmail.com>
2018-12-04 13:26:36 +0100

    correcting a function name

--- coda-io-connection-doc-id.js
+++ coda-io-connection-doc-id.js
@@ -1,7 +1,7 @@
 module.exports = function(RED) {
-    function CodaIoConnectionTableIdNode(n) {
+    function CodaIoConnectionDocIdNode(n) {
         RED.nodes.createNode(this, n);
         this.doc_id = n.doc_id;
     }
-    RED.nodes.registerType("coda-io-connection-doc-id", CodaIoConnectionTableIdNode);
+    RED.nodes.registerType("coda-io-connection-doc-id", CodaIoConnectionDocIdNode);
 }
