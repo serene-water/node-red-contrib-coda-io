d1b25dbad065a878733f8be30b3058a88444d23d
Mori Sugimoto <morihiro.sugimoto@gmail.com>
2018-12-10 04:51:42 +0100

    API keys are no longer exported along with nodes/flows for improved security

--- coda-io-connection-api-token.js
+++ coda-io-connection-api-token.js
@@ -1,7 +1,13 @@
 module.exports = function(RED) {
     function CodaIoConnectionApiTokenNode(n) {
-        RED.nodes.createNode(this,n);
-        this.api_token = n.api_token;
+        RED.nodes.createNode(this, n);
+        this.api_token = this.credentials.api_token;
     }
-    RED.nodes.registerType("coda-io-connection-api-token", CodaIoConnectionApiTokenNode);
+    RED.nodes.registerType("coda-io-connection-api-token", CodaIoConnectionApiTokenNode, {
+        // Store the credential separate to the main flow file
+        // so it won't be exported along with the flow
+        credentials: {
+            api_token: {type:"api_token"}
+        }
+    });
 }
